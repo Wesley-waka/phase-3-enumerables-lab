7b4e28293266793ee607173b223bb6f024fe20c2
Wesley-waka <wesley.waka@student.moringaschool.com>
2023-02-09 06:23:14 +0300

    Done

--- enumerables.rb
+++ enumerables.rb
@@ -13,12 +13,17 @@ end
 # with the names of each spicy food
 def get_names(spicy_foods)
   # your code here
+  return spicy_foods.map{|x| x[:name]}
 end
+# get_names(spicy_foods)
 
 # given an array of spicy foods, **return an array of hashes** 
 # where the heat level of the food is greater than 5
 def spiciest_foods(spicy_foods)
   # your code here
+  # return spicy_foods.map{|x| x[:heat_level] x[:heat_level]}
+  return spicy_foods.select{|x| x[:heat_level]>5}
+
 end
 
 # given an array of spicy foods, **output to the terminal**
@@ -28,18 +33,24 @@ end
 # "hello" * 3 == "hellohellohello"
 def print_spicy_foods(spicy_foods)
   # your code here
+  spicy_foods.map do |spicy|
+    puts "#{spicy[:name]} (#{spicy[:cuisine]}) | Heat Level: #{"🌶" * spicy[:heat_level]}"
 end
+
 
 # given an array of spicy foods and a string representing a cuisine, **return a single hash**  
 # for the spicy food whose cuisine matches the cuisine being passed to the method
 def get_spicy_food_by_cuisine(spicy_foods, cuisine)
   # your code here
+  spicy_foods.find { |key| key[:cuisine] == cuisine }
+
 end
 
 # Given an array of spicy foods, **return an array of hashes** 
 # sorted by heat level from lowest to highest
 def sort_by_heat(spicy_foods)
   # your code here
+  return spicy_foods.map{|x| x}.sort_by{|x| x[:heat_level]}
 end
 
 # given an array of spicy foods, output to the terminal ONLY 
@@ -48,10 +59,15 @@ end
 # HINT: Try to use methods you've already written to solve this!
 def print_spiciest_foods(spicy_foods)
   # your code here
+  spiciest = spiciest_foods(spicy_foods)
+  print_spicy_foods(spiciest)
 end
 
 # given an array of spicy foods, return an integer representing 
 # the average heat level of all the spicy foods in the array
 def average_heat_level(spicy_foods)
   # your code here
+  avg=spicy_foods.sum{|food_heat| food_heat[:heat_level]}
+  return avg/spicy_foods.length
+end
 end
